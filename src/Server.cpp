7d6de5042747d2bcdd5fccbf45db586b0b4c51b9
Tim@Davis-Dorm-WSL2 <timhwang@ucdavis.edu>
2024-01-03 15:01:26 -0800

    1st stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -3,43 +3,43 @@
 #include <fstream>
 #include <string>
 
+using namespace std;
+
 int main(int argc, char* argv[]) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cout << "Logs from your program will appear here!\n";
+    cout << "Logs from your program will appear here!\n";
 
-    // Uncomment this block to pass the first stage
-    //
-    // if (argc < 2) {
-    //     std::cerr << "No command provided.\n";
-    //     return EXIT_FAILURE;
-    // }
-    //
-    // std::string command = argv[1];
-    //
-    // if (command == "init") {
-    //     try {
-    //         std::filesystem::create_directory(".git");
-    //         std::filesystem::create_directory(".git/objects");
-    //         std::filesystem::create_directory(".git/refs");
-    //
-    //         std::ofstream headFile(".git/HEAD");
-    //         if (headFile.is_open()) {
-    //             headFile << "ref: refs/heads/master\n";
-    //             headFile.close();
-    //         } else {
-    //             std::cerr << "Failed to create .git/HEAD file.\n";
-    //             return EXIT_FAILURE;
-    //         }
-    //        
-    //         std::cout << "Initialized git directory\n";
-    //     } catch (const std::filesystem::filesystem_error& e) {
-    //         std::cerr << e.what() << '\n';
-    //         return EXIT_FAILURE;
-    //     }
-    // } else {
-    //     std::cerr << "Unknown command " << command << '\n';
-    //     return EXIT_FAILURE;
-    // }
-    //
-    // return EXIT_SUCCESS;
+    if (argc < 2) {
+        cerr << "No command provided.\n";
+        return EXIT_FAILURE;
+    }
+
+    string command = argv[1];
+
+    if (command == "init") {
+        try {
+            filesystem::create_directory(".git");
+            filesystem::create_directory(".git/objects");
+            filesystem::create_directory(".git/refs");
+
+            ofstream headFile(".git/HEAD");
+            if (headFile.is_open()) {
+                headFile << "ref: refs/heads/master\n";
+                headFile.close();
+            } else {
+                cerr << "Failed to create .git/HEAD file.\n";
+                return EXIT_FAILURE;
+            }
+           
+            cout << "Initialized git directory\n";
+        } catch (const filesystem::filesystem_error& e) {
+            cerr << e.what() << '\n';
+            return EXIT_FAILURE;
+        }
+    } else {
+        cerr << "Unknown command " << command << '\n';
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
