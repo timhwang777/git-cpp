f70e3ecbf3042626f90da66d526e2bc9d205f850
Tim@Davis-Dorm-WSL2 <timhwang@ucdavis.edu>
2024-01-04 11:10:55 -0800

    2nd stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,14 +1,80 @@
+/* Default */
 #include <iostream>
 #include <filesystem>
 #include <fstream>
 #include <string>
+/* User defined */
+#include <cstring>
+#include <zlib.h>
 
+#define CHUNK 16384 //16KB
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    cout << "Logs from your program will appear here!\n";
+/* Functions */
+int decompress(FILE* input, FILE* output) {
+    // initialize decompression stream
+    z_stream stream = {0};
+    if (inflateInit(&stream) != Z_OK) {
+        cerr << "Failed to initialize decompression stream.\n";
+        return EXIT_FAILURE;
+    }
 
+    // initialize decompression variables
+    char in[CHUNK];
+    char out[CHUNK];
+    bool haveHeader = false;
+    char header[64];
+    int ret;
+
+    do {
+        stream.avail_in = fread(in, 1, CHUNK, input); // read from input file
+        stream.next_in = reinterpret_cast<unsigned char*>(in); // set input stream
+        if (ferror(input)) {
+            cerr << "Failed to read from input file.\n";
+            return EXIT_FAILURE;
+        }
+        if (stream.avail_in == 0) {
+            break;
+        }
+
+        do {
+            stream.avail_out = CHUNK; // set output buffer size
+            stream.next_out = reinterpret_cast<unsigned char*>(out); // set output stream
+            ret = inflate(&stream, Z_NO_FLUSH); // decompress
+            if (ret == Z_NEED_DICT || ret == Z_DATA_ERROR || ret == Z_MEM_ERROR) {
+                cerr << "Failed to decompress file.\n";
+                return EXIT_FAILURE;
+            }
+
+            // write header to output file
+            if (!haveHeader) {
+                unsigned headerLen = CHUNK - stream.avail_out; // get header size
+                if(headerLen > 0) {
+                    haveHeader = true;
+                    memcpy(header, out, headerLen);
+                    if(fwrite(header, 1, headerLen, output) != headerLen) {
+                        cerr << "Failed to write header to output file.\n";
+                        return EXIT_FAILURE;
+                    }
+                }
+            }
+
+            // write decompressed data to output file
+            if (stream.avail_out < CHUNK) {
+                unsigned dataLen = CHUNK - stream.avail_out;
+                if(fwrite(out, 1, dataLen, output) != dataLen) {
+                    cerr << "Failed to write decompressed data to output file.\n";
+                    return EXIT_FAILURE;
+                }
+            }
+        } while (stream.avail_out == 0);
+        
+    } while (ret != Z_STREAM_END);
+
+    return inflateEnd(&stream) == Z_OK ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+int main(int argc, char* argv[]) {
     if (argc < 2) {
         cerr << "No command provided.\n";
         return EXIT_FAILURE;
@@ -23,8 +89,8 @@ int main(int argc, char* argv[]) {
             filesystem::create_directory(".git/refs");
 
             ofstream headFile(".git/HEAD");
-            if (headFile.is_open()) {
-                headFile << "ref: refs/heads/master\n";
+            if (headFile.is_open()) { // create .git/HEAD file
+                headFile << "ref: refs/heads/master\n"; // write to the headFile
                 headFile.close();
             } else {
                 cerr << "Failed to create .git/HEAD file.\n";
@@ -36,7 +102,37 @@ int main(int argc, char* argv[]) {
             cerr << e.what() << '\n';
             return EXIT_FAILURE;
         }
-    } else {
+    }
+    else if (command == "git-cat") {
+        // check if object hash is provided
+        if (argc < 3) {
+            cerr << "No object hash provided.\n";
+            return EXIT_FAILURE;
+        }
+
+        // retrieve file path and check if object hash is valid
+        char dataPath[64];
+        snprintf(dataPath, sizeof(dataPath), ".git/objects/%.2s/%s", argv[3], argv[3] + 2);
+        FILE* dataFile = fopen(dataPath, "rb");
+        if (!dataFile) {
+            cerr << "Invalid object hash.\n";
+            return EXIT_FAILURE;
+        }
+
+        // create output file
+        FILE* outputFile = fdopen(1, "w");
+        if (!outputFile) {
+            cerr << "Failed to create output file.\n";
+            return EXIT_FAILURE;
+        }
+
+        // decompress data file
+        if (decompress(dataFile, outputFile) != EXIT_SUCCESS) {
+            cerr << "Failed to decompress data file.\n";
+            return EXIT_FAILURE;
+        }
+    }
+    else {
         cerr << "Unknown command " << command << '\n';
         return EXIT_FAILURE;
     }
