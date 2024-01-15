acdc9fd1b64f9e09c7271e55512250a5e6a9b564
Tim@Davis-Dorm-WSL2 <timhwang@ucdavis.edu>
2024-01-15 11:26:54 -0800

    6th stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -288,8 +288,8 @@ std::string commit_tree (std::string tree_sha, std::string parent_sha, std::stri
 
     std::string commit_content = "tree " + tree_sha + "\n" +
                                  "parent " + parent_sha + "\n" +
-                                 "author " + author + " " + timestamp + " +0000\n" +
-                                 "committer " + committer + " " + timestamp + " +0000\n" +
+                                 "author " + author + " " + timestamp + " -0800\n" +
+                                 "committer " + committer + " " + timestamp + " -0800\n" +
                                  "\n" + message + "\n";
 
     std::string header = "commit " + std::to_string(commit_content.size()) + "\0";
